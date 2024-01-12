86fed74335c819b2b8c38c883d5a8189d4dc34fe
NguyenAnhDuong <nguyenanhduong.01284033022@gmail.com>
2024-01-12 19:38:12 +0700

    Nộp bài kiểm tra

---
+++ Demo01/src/consts/colors.js
@@ -0,0 +1,16 @@
+const COLORS = {
+  primary: '#122636',
+  white: '#FFF',
+  dark: '#000',
+  light: '#f7f7f7',
+  grey: '#A9A9A9',
+  yellow: '#ffb900',
+  red: 'red',
+  green: '#008000',
+  tranparent: 'rgba(0,0,0,0)',
+  greenyellow: '#adff2f',
+  darkgreen: '#006400',
+  lightgreen: '#90ee90'
+};
+
+export default COLORS;
